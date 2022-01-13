547653eb9ca81b9f59637e247a3b9c8e29e3c9b0
Jenny Strømmen <jennys1994@gmail.com>
2022-01-13 12:27:54 +0100

    Use api instead of dependency

--- master.thesis.eclipse.plugin/src/master/thesis/eclipse/plugin/handlers/ErrorHandler.java
+++ master.thesis.eclipse.plugin/src/master/thesis/eclipse/plugin/handlers/ErrorHandler.java
@@ -1,20 +1,25 @@
 package master.thesis.eclipse.plugin.handlers;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 
-import master.thesis.backend.analyser.Analyser;
 import editor.Editor;
-import master.thesis.backend.errors.BaseError;
-import master.thesis.backend.errors.BugReport;
 
 public class ErrorHandler extends AbstractHandler {
+	
+	private static final String CONTAINING_CLASS_KEY = "\"containingClass\":";
+	private static final String ERRORS = "\"status\":\"errors\"";
+	private static final String LINENUMBER_KEY = "\"lineNumber\":";
+	private static final String EXPLANATION_KEY = "\"explanation\":";
+	private static final String SUGGESTION_KEY = "\"suggestion\":";
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
@@ -24,40 +29,51 @@ public class ErrorHandler extends AbstractHandler {
 		
 		IFile file = Editor.getSelectedFile();
 		
-		String code = "";
 		try {
-			code = new String(file.getContents().readAllBytes(), StandardCharsets.UTF_8);
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (CoreException e) {
-			e.printStackTrace();
-		}
-		
-		Analyser analyser = new Analyser();
-		BugReport bugReport = analyser.analyse(code);
-		
-		if (!bugReport.getBugs().isEmpty()) {
-			BaseError error = bugReport.getBugs().get(0);
-			if (error.getLineNumber() != -1) {
-				Editor.printToConsole("In file: " + file.getName() + ", in class " + error.getContainingClass() + ", at line " + error.getLineNumber() + ":");
-			} else {
-				Editor.printToConsole("In file: " + file.getName() + "in class " + error.getContainingClass()  + ":");
-			}
-			
-			Editor.printToConsole(error.getWhat());
-			
-			if (error.getSuggestion().isPresent()) {
-				Editor.printToConsole(" ");
-				Editor.printToConsole(error.getSuggestion().get());
-			}
-			
-			Editor.openFile(file);
-			Editor.findAndMarkText(error.getOffset(), error.getLength());
-			return null;
-		} 
-	
-		
-		Editor.printToConsole("Found no errors!");
+            URL url = new URL("https://master-thesis-web-backend-prod.herokuapp.com/analyse");
+            URLConnection con = url.openConnection();
+            HttpURLConnection http = (HttpURLConnection) con;
+            http.setRequestMethod("POST");
+            http.setDoOutput(true);
+            http.setRequestProperty("Content-Type", "text/plain; charset=UTF-8");
+            http.connect();
+
+            try(OutputStream os = http.getOutputStream()) {
+                try {
+					os.write(file.getContents().readAllBytes());
+				} catch (CoreException e) {
+					e.printStackTrace();
+				}
+            }
+            String response = new String(http.getInputStream().readAllBytes());
+            
+            if (response.contains(ERRORS)) {
+                String result = "In class " + getValue(CONTAINING_CLASS_KEY, response);
+                if (Integer.parseInt(getValue(LINENUMBER_KEY, response)) != -1) {
+                   result += ", on line number " + getValue(LINENUMBER_KEY, response) + "\n";
+                }
+                result += getValue(EXPLANATION_KEY, response) + "\n";
+                if (response.contains(SUGGESTION_KEY)) {
+                    result += getValue(SUGGESTION_KEY, response);
+                }
+                Editor.printToConsole(result);
+            } else {
+            	Editor.printToConsole("Found no errors!");
+            }
+            
+
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
 		return null;
+	}
+	
+	private String getValue(String KEY, String obj) {
+		String sub = obj.substring(obj.indexOf(KEY)+KEY.length());
+		String val = sub.substring(0, sub.indexOf(","));
+		String valStripped = val.replace('"', ' ');
+		return valStripped;
 	}
 }
